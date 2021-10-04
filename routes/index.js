8e459412fe8c16a8648bc7e2fb80d8430d388492
Lirim Krosa <krosalirim@gmail.com>
2021-10-04 12:00:00 +0200

    style: format code

--- routes/index.js
+++ routes/index.js
@@ -1,47 +1,33 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var gplay = require('google-play-scraper');
-const { IncomingWebhook } = require('@slack/webhook');
+var gplay = require("google-play-scraper");
+const { IncomingWebhook } = require("@slack/webhook");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-
-  
-  res.send({ title: 'it works!' });
+router.get("/", function (req, res, next) {
+  res.send({ title: "it works!" });
 });
 
-
-router.get('/api', function(req, res, next) {
-
-
+router.get("/api", function (req, res, next) {
   const url = "SLACK_WEBHOOK_URL";
   const webhook = new IncomingWebhook(url);
-  gplay.search({
-    term: "KEYWORD",
-    num: 250
-  }).then((result)=>{
-
-    result.map((value)=>{
-
-      if (value.title.includes("KEYWORD") && value.appId != "EXCLUDE_APP"){
-
-        (async () => {
-          await webhook.send({
-            text: value.title+ ` | URL: ${value.url}`
-          })
-        })();
-      }
-
-     
+  gplay
+    .search({
+      term: "KEYWORD",
+      num: 250,
     })
-    res.send(result)
-    
-  }
-
-  
-
-  );
-
+    .then((result) => {
+      result.map((value) => {
+        if (value.title.includes("KEYWORD") && value.appId != "EXCLUDE_APP") {
+          (async () => {
+            await webhook.send({
+              text: value.title + ` | URL: ${value.url}`,
+            });
+          })();
+        }
+      });
+      res.send(result);
+    });
 });
 
 module.exports = router;
