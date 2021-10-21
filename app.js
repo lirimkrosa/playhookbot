c87ff8f5dbcc936d1d8a8fca2ea060fac6115c4a
Lirim Krosa <krosalirim@gmail.com>
2021-10-22 00:19:37 +0200

    refactor: optimize routes and error handling

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require("./routes/index");
+const indexRouter = require("./routes/index");
 
-var app = express();
-
-// view engine setup
+const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -18,19 +16,22 @@ app.use(cookieParser());
 app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
+  // return JSON error response
   res.status(err.status || 500);
-  res.render("error");
+  res.json({
+    message: err.message,
+    error: req.app.get("env") === "development" ? err : {},
+  });
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,41 @@
-var express = require("express");
-var router = express.Router();
-var gplay = require("google-play-scraper");
+const express = require("express");
+const router = express.Router();
+const gplay = require("google-play-scraper");
 const { IncomingWebhook } = require("@slack/webhook");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/", (req, res) => {
   res.send({ title: "it works!" });
 });
 
-router.get("/api", function (req, res, next) {
-  const url = "SLACK_WEBHOOK_URL";
-  const webhook = new IncomingWebhook(url);
-  gplay
-    .search({
-      term: "KEYWORD",
+router.get("/api", async (req, res, next) => {
+  try {
+    const url = process.env.SLACK_WEBHOOK_URL || "SLACK_WEBHOOK_URL";
+    const keyword = process.env.SEARCH_KEYWORD || "KEYWORD";
+    const excludeAppId = process.env.EXCLUDE_APP_ID || "EXCLUDE_APP";
+
+    const webhook = new IncomingWebhook(url);
+
+    const results = await gplay.search({
+      term: keyword,
       num: 250,
-    })
-    .then((result) => {
-      result.map((value) => {
-        if (value.title.includes("KEYWORD") && value.appId != "EXCLUDE_APP") {
-          (async () => {
-            await webhook.send({
-              text: value.title + ` | URL: ${value.url}`,
-            });
-          })();
-        }
-      });
-      res.send(result);
     });
+
+    const matchingApps = results.filter(
+      (app) => app.title.includes(keyword) && app.appId !== excludeAppId,
+    );
+
+    // Send notifications to Slack concurrently
+    await Promise.all(
+      matchingApps.map((app) =>
+        webhook.send({ text: `${app.title} | URL: ${app.url}` }),
+      ),
+    );
+
+    res.send(results);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
